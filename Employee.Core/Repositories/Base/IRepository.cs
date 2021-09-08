2678f23822f35065a59c80a53b573e4b509b21d5
Nkechi Okeke <janecutie89@gmail.com>
2021-09-08 15:55:48 +0100

    initial commit

---
+++ Employee.Core/Repositories/Base/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Core.Repositories.Base
+{
+    public interface IRepository<T> where T : class 
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> GetByAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+    }
+}
