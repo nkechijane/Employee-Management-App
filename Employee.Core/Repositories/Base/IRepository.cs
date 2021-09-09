e90886325c24128308a9a8e4ac792afe8285494b
Nkechi Okeke <janecutie89@gmail.com>
2021-09-09 18:19:32 +0100

    added repository folder and create Irepository interface

--- Employee.Core/Repositories/Base/IRepository.cs
+++ Employee.Core/Repositories/Base/IRepository.cs
@@ -9,9 +9,9 @@ namespace Employee.Core.Repositories.Base
     public interface IRepository<T> where T : class 
     {
         Task<IReadOnlyList<T>> GetAllAsync();
-        Task<T> GetByAsync(int id);
+        Task<T> GetByIdAsync(int id);
         Task<T> AddAsync(T entity);
-        Task<T> UpdateAsync(T entity);
-        Task<T> DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
